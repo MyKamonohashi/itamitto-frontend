7ac172061ba7775d0bb779993fa3888a9030cfe7
nikobatzi1990 <nicole.boci@gmail.com>
2024-03-07 16:22:52 +0900

    feat: import buttons and localization

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,19 @@
 import { registerRootComponent } from 'expo';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import en from './localize/en.json';
+import ja from './localize/ja.json'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>{en.title.reason}</Text>
+      <View>
+        <Button title={en.reason.illness}></Button>
+        <Button title={en.reason.injury}></Button>
+        <Button title={en.reason.vaccine}></Button>
+        <Button title={en.reason.test}></Button>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
