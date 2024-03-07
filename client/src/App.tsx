9a34c50fca564af5e7689a4bc52a40139fe7456c
nikobatzi1990 <nicole.boci@gmail.com>
2024-03-07 16:48:50 +0900

    feat: add handler for language state

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,29 @@
 import { registerRootComponent } from 'expo';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button } from 'react-native';
+import { useState } from 'react';
 import en from './localize/en.json';
 import ja from './localize/ja.json'
 
 export default function App() {
+  const [language, setLanguage] = useState(en);
+
+  const handleLanguage = () => {
+    if (language === en) {
+      setLanguage(ja);
+    } else {
+      setLanguage(en);
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>{en.title.reason}</Text>
+      <Text>{language.title.reason}</Text>
       <View>
-        <Button title={en.reason.illness}></Button>
-        <Button title={en.reason.injury}></Button>
-        <Button title={en.reason.vaccine}></Button>
-        <Button title={en.reason.test}></Button>
+        <Button title={language.reason.illness}></Button>
+        <Button title={language.reason.injury}></Button>
+        <Button title={language.reason.vaccine}></Button>
+        <Button title={language.reason.test}></Button>
       </View>
       <StatusBar style="auto" />
     </View>
