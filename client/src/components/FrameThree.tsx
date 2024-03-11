08422109c6d9c08789dd6138bfb3e8e0a0b6312f
nikobatzi1990 <nicole.boci@gmail.com>
2024-03-11 22:42:58 +0900

    feat: create setup for FrameThree

---
+++ client/src/components/FrameThree.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View, Pressable, Switch } from 'react-native';
+import { useState, useEffect } from 'react';
+
+export default function FrameThree() {
+  return (
+    <View>
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+
+});
