811556916e1429c58a57850a7318e0d93ff6d466
nikobatzi1990 <nicole.boci@gmail.com>
2024-03-12 23:37:39 +0900

    feat: add buttons

--- client/src/components/FrameThree.tsx
+++ client/src/components/FrameThree.tsx
@@ -26,7 +26,36 @@ export default function FrameThree() {
     <View>
       <Text>{language.title.pain_location}</Text>
       <View>
-        
+        <Pressable>
+          <Text>{language.pain_location.head}</Text>
+        </Pressable>
+        <Pressable>
+          <Text>{language.pain_location.stomach}</Text>
+        </Pressable>
+        <Pressable>
+          <Text>{language.pain_location.arm}</Text>
+        </Pressable>
+        <Pressable>
+          <Text>{language.pain_location.leg}</Text>
+        </Pressable>
+        <Pressable>
+          <Text>{language.pain_location.back}</Text>
+        </Pressable>
+        <Pressable>
+          <Text>{language.pain_location.shoulder}</Text>
+        </Pressable>
+        <Pressable>
+          <Text>{language.pain_location.neck}</Text>
+        </Pressable>
+        <Pressable>
+          <Text>{language.pain_location.chest}</Text>
+        </Pressable>
+        <Pressable>
+          <Text>{language.pain_location.eyes_ears_nose}</Text>
+        </Pressable>
+        <Pressable>
+          <Text>{language.button.submit}</Text>
+        </Pressable>
       </View>
     </View>
   );
