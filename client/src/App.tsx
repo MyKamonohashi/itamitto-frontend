ad6a9f569df6973a8cdfe607b660ee23565fe814
ikuno815 <ikuno.kanasugi19@gmail.com>
2024-03-13 01:43:35 +0900

    feat: add stack navigation in App.tsx[wip]

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,42 @@
 import { registerRootComponent } from 'expo';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View} from 'react-native';
+// import { StatusBar } from 'expo-status-bar';
+// import { StyleSheet, View} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import FrameOne from './components/FrameOne'
+import FrameTwo from './components/FrameTwo';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="FrameOne"
+        screenOptions={{
+        headerTitle: 'itamitto',
+        headerTitleAlign: 'center'
+      }}>
+        <Stack.Screen name="FrameOne" component={FrameOne}/>
+        <Stack.Screen name="FrameTwo" component={FrameTwo}/>
+    {/* <View style={styles.container}>
       <View>
         <FrameOne />
       </View>
       <StatusBar style="auto" />
-    </View>
+    </View> */}
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   }, 
+// });
 
 registerRootComponent(App);
