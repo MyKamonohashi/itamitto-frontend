0aa2b8dc6dddd5deafa7d2eb18325f37585f8dd3
nikobatzi1990 <nicole.boci@gmail.com>
2024-03-14 16:07:02 +0900

    feat: checkbox inputs for pain descriptions

--- client/src/components/FrameFour.tsx
+++ client/src/components/FrameFour.tsx
@@ -26,24 +26,27 @@ interface FrameProps {
 }
 
 export default function FrameFour({language}: FrameProps) {
-  const [checked, setChecked] = useState(false);
-
-  const handleChecked = () => {
-    setChecked(!checked);
-  }
+  const [checked1, setChecked1] = useState(false);
+  const [checked2, setChecked2] = useState(false);
+  const [checked3, setChecked3] = useState(false);
+  const [checked4, setChecked4] = useState(false);
+  const [checked5, setChecked5] = useState(false);
+  const [checked6, setChecked6] = useState(false);
+  const [checked7, setChecked7] = useState(false);
+  const [checked8, setChecked8] = useState(false);
 
   return (
     <View style={styles.container}>
       <Text>{language.title.pain_description}</Text>
       <View>
-        <CheckBox checked={checked} title={language.pain_description.sharp} onPress={handleChecked}/>
-        <CheckBox checked={checked} title={language.pain_description.throbbing}/>
-        <CheckBox checked={checked} title={language.pain_description.aching}/>
-        <CheckBox checked={checked} title={language.pain_description.burning}/>
-        <CheckBox checked={checked} title={language.pain_description.cramping}/>
-        <CheckBox checked={checked} title={language.pain_description.numbness}/>
-        <CheckBox checked={checked} title={language.pain_description.numbness}/>
-        <CheckBox checked={checked} title={language.pain_description.constant}/>
+        <CheckBox checked={checked1} title={language.pain_description.sharp} onPress={() => {setChecked1(!checked1)}}/>
+        <CheckBox checked={checked2} title={language.pain_description.throbbing} onPress={() => {setChecked2(!checked2)}}/>
+        <CheckBox checked={checked3} title={language.pain_description.aching} onPress={() => {setChecked3(!checked3)}}/>
+        <CheckBox checked={checked4} title={language.pain_description.burning} onPress={() => {setChecked4(!checked4)}}/>
+        <CheckBox checked={checked5} title={language.pain_description.cramping} onPress={() => {setChecked5(!checked5)}}/>
+        <CheckBox checked={checked6} title={language.pain_description.numbness} onPress={() => {setChecked6(!checked6)}}/>
+        <CheckBox checked={checked7} title={language.pain_description.constant} onPress={() => {setChecked7(!checked7)}}/>
+        <CheckBox checked={checked8} title={language.pain_description.sudden} onPress={() => {setChecked8(!checked8)}}/>
       </View>
     </View>
   );
