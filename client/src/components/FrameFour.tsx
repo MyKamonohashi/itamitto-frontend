89c6380d7042a751538e9f54661d21ec776a298f
nikobatzi1990 <nicole.boci@gmail.com>
2024-03-15 18:36:38 +0900

    feat: handle checkbox data on submission

--- client/src/components/FrameFour.tsx
+++ client/src/components/FrameFour.tsx
@@ -34,6 +34,36 @@ export default function FrameFour({language}: FrameProps) {
   const [checked6, setChecked6] = useState(false);
   const [checked7, setChecked7] = useState(false);
   const [checked8, setChecked8] = useState(false);
+  const [data, setData] = useState<string[]>([]);
+
+  const handleDataInput = () => {
+    const result = [];
+    if (checked1) {
+      result.push(language.pain_description.sharp);
+    }
+    if (checked2) {
+      result.push(language.pain_description.throbbing);
+    }
+    if (checked3) {
+      result.push(language.pain_description.aching);
+    }
+    if (checked4) {
+      result.push(language.pain_description.burning);
+    }
+    if (checked5) {
+      result.push(language.pain_description.cramping);
+    }
+    if (checked6) {
+      result.push(language.pain_description.numbness);
+    }
+    if (checked7) {
+      result.push(language.pain_description.constant);
+    }
+    if (checked8) {
+      result.push(language.pain_description.sudden);
+    }
+    setData(result);
+  }
 
   return (
     <View style={styles.container}>
@@ -48,7 +78,7 @@ export default function FrameFour({language}: FrameProps) {
         <CheckBox checked={checked7} title={language.pain_description.constant} onPress={() => {setChecked7(!checked7)}}/>
         <CheckBox checked={checked8} title={language.pain_description.sudden} onPress={() => {setChecked8(!checked8)}}/>
       </View>
-      <Pressable>
+      <Pressable onPress={handleDataInput}>
         <Text>{language.button.submit}</Text>
       </Pressable>
     </View>
