6a32c826e4cdec1e6fde710ae3bdf98dc396e017
nikobatzi1990 <nicole.boci@gmail.com>
2024-03-20 17:09:32 +0900

    feat: add checkbox inputs

--- client/src/components/FrameSeven.tsx
+++ client/src/components/FrameSeven.tsx
@@ -19,6 +19,17 @@ export default function FrameSeven({ route, navigation }: FrameSevenProps) {
   const [language, setLanguage] = useState(route.params.language);
   const [isEnabled, setIsEnabled] = useState(route.params.isEnabled);
   const toggleSwitch = () => setIsEnabled(previousState => !previousState);
+  const [tests, setTests] = useState('');
+
+  const [checked1, setChecked1] = useState(false);
+  const [checked2, setChecked2] = useState(false);
+  const [checked3, setChecked3] = useState(false);
+  const [checked4, setChecked4] = useState(false);
+  const [checked5, setChecked5] = useState(false);
+  const [checked6, setChecked6] = useState(false);
+  const [checked7, setChecked7] = useState(false);
+  const [checked8, setChecked8] = useState(false);
+  const [checked9, setChecked9] = useState(false);
 
   const handleLanguage = () => {
     if (!isEnabled) {
@@ -33,9 +44,29 @@ export default function FrameSeven({ route, navigation }: FrameSevenProps) {
   }, [isEnabled]);
   
   return (
-    <View>
+    <View style={styles.container}>
       <Text>{language.title.test}</Text>
+      <View>
+        <CheckBox checked={checked1} title={language.test.blood_test} onPress={() => {setChecked1(!checked1)}}/>
+        <CheckBox checked={checked2} title={language.test.urine_test} onPress={() => {setChecked2(!checked2)}}/>
+        <CheckBox checked={checked3} title={language.test.allergy_test} onPress={() => {setChecked3(!checked3)}}/>
+        <CheckBox checked={checked4} title={language.test.ultrasound} onPress={() => {setChecked4(!checked4)}}/>
+        <CheckBox checked={checked5} title={language.test.STD_test} onPress={() => {setChecked5(!checked5)}}/>
+        <CheckBox checked={checked6} title={language.test.X_ray} onPress={() => {setChecked6(!checked6)}}/>
+        <CheckBox checked={checked7} title={language.test.mammogram} onPress={() => {setChecked7(!checked7)}}/>
+        <CheckBox checked={checked8} title={language.test.pap_smear} onPress={() => {setChecked8(!checked8)}}/>
+        <CheckBox checked={checked9} title={language.test.MRI_CAT_scan} onPress={() => {setChecked8(!checked9)}}/>
+      </View>
       <LanguageToggle onValueChange={toggleSwitch} isEnabled={isEnabled}/>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'space-around'
+  },
+});
