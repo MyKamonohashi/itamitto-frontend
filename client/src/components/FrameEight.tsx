a93a63b8d3c5ac5ae995fd13573f9071ab2bfc39
nikobatzi1990 <nicole.boci@gmail.com>
2024-03-20 17:46:32 +0900

    feat: handle frame8 functionality

--- client/src/components/FrameEight.tsx
+++ client/src/components/FrameEight.tsx
@@ -1,5 +1,6 @@
 import { StyleSheet, Text, View, Pressable } from 'react-native';
 import { useState, useEffect } from 'react';
+import { CheckBox } from '@rneui/themed';
 import en from '../localize/en';
 import ja from '../localize/ja';
 import LanguageToggle from './LanguageToggle';
@@ -17,6 +18,11 @@ export default function FrameEight({ route, navigation }: FrameEightProps) {
   const toggleSwitch = () => setIsEnabled(previousState => !previousState);
   const [vaccines, setVaccines] = useState('');
 
+  const [checked1, setChecked1] = useState(false);
+  const [checked2, setChecked2] = useState(false);
+  const [checked3, setChecked3] = useState(false);
+  const [checked4, setChecked4] = useState(false);
+
   const handleLanguage = () => {
     if (!isEnabled) {
       setLanguage(en);
@@ -29,9 +35,32 @@ export default function FrameEight({ route, navigation }: FrameEightProps) {
     handleLanguage();
   }, [isEnabled]);
 
+  const handleDataInput = () => {
+    const result = [];
+    if (checked1) {
+      result.push(language.vaccine.influenza);
+    }
+    if (checked2) {
+      result.push(language.vaccine.COVID_19);
+    }
+    if (checked3) {
+      result.push(language.vaccine.HPV);
+    }
+    if (checked4) {
+      result.push(language.vaccine.measles);
+    }
+    setVaccines(result.join('/'));
+  }
+
   return (
     <View style={styles.container}>
       <Text>{language.reason.vaccine}</Text>
+      <View>
+      <CheckBox checked={checked1} title={language.vaccine.influenza} onPress={() => {setChecked1(!checked1)}}/>
+        <CheckBox checked={checked2} title={language.vaccine.COVID_19} onPress={() => {setChecked2(!checked2)}}/>
+        <CheckBox checked={checked3} title={language.vaccine.HPV} onPress={() => {setChecked3(!checked3)}}/>
+        <CheckBox checked={checked4} title={language.vaccine.measles} onPress={() => {setChecked4(!checked4)}}/>
+      </View>
       <LanguageToggle onValueChange={toggleSwitch} isEnabled={isEnabled}/>
       <Pressable
       // uncomment when FrameNine is created to navigate
