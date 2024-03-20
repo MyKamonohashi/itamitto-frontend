da2d860f0a226ef9eb63018931146fd129db04cb
ikuno815 <ikuno.kanasugi19@gmail.com>
2024-03-20 22:30:06 +0900

    fix: modify objFinalReport

--- client/src/components/ObjectFinalReport.tsx
+++ client/src/components/ObjectFinalReport.tsx
@@ -1,7 +1,13 @@
 const objFinalReport = {
     symptoms: "no input",
     pain: "no input",
+    pain_description: "no input",
+    pain_scale: "no input",
     duration: "no input",
+    other_medication: "no input",
+    pregnancy: "no input",
+    allergies: "no input",
+    allergies_name: "no input",
     test: "no input",
     vaccine: "no input",
     special_notes: "no input"
