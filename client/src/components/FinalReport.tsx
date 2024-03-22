eaae13fa4ca3c01874bf2bea4faf5ce1c5df7683
ikuno815 <ikuno.kanasugi19@gmail.com>
2024-03-22 15:34:47 +0900

    feat: add LanguageToggle to final report

--- client/src/components/FinalReport.tsx
+++ client/src/components/FinalReport.tsx
@@ -76,6 +76,7 @@ export default function FinalReport({ route }: FinalReportProps) {
         </View>
         )
         )}
+        <LanguageToggle onValueChange={toggleSwitch} isEnabled={isEnabled}/>
     </View>
   );
 }
