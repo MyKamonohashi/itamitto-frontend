03043cd29e12a1e6c412a2c605000fa4d1c07db5
nikobatzi1990 <nicole.boci@gmail.com>
2024-03-23 10:35:59 +0900

    feat: handle inputs and data submission

--- client/src/components/FrameFifteen.tsx
+++ client/src/components/FrameFifteen.tsx
@@ -32,8 +32,44 @@ export default function FrameFifteen( { route, navigation }: FrameFifteenProps) 
     <View style={styles.container}>
       <Text>{language.title.injury_location}</Text>
       <View>
-  
+      <Pressable onPress={() => setInjuryLocation("head")}>
+          <Text>{language.injury_location.head}</Text>
+        </Pressable>
+        <Pressable onPress={() => setInjuryLocation("stomach")}>
+          <Text>{language.injury_location.stomach}</Text>
+        </Pressable>
+        <Pressable onPress={() => setInjuryLocation("arm")}>
+          <Text>{language.injury_location.arm}</Text>
+        </Pressable>
+        <Pressable onPress={() => setInjuryLocation("leg")}>
+          <Text>{language.injury_location.leg}</Text>
+        </Pressable>
+        <Pressable onPress={() => setInjuryLocation("back")}>
+          <Text>{language.injury_location.back}</Text>
+        </Pressable>
+        <Pressable onPress={() => setInjuryLocation("shoulder")}>
+          <Text>{language.injury_location.shoulder}</Text>
+        </Pressable>
+        <Pressable onPress={() => setInjuryLocation("neck")}>
+          <Text>{language.injury_location.neck}</Text>
+        </Pressable>
+        <Pressable onPress={() => setInjuryLocation("chest")}>
+          <Text>{language.injury_location.chest}</Text>
+        </Pressable>
+        <Pressable onPress={() => setInjuryLocation("eyes/ears/nose")}>
+          <Text>{language.injury_location.eyes_ears_nose}</Text>
+        </Pressable>
       </View>
+      {/* <Pressable onPress={() => {
+          // go to FrameSixteen
+          navigation.navigate("FrameSixteen", {
+            injury_location: injuryLocation,
+            language: language,
+            isEnabled: isEnabled})
+          }
+        }>
+          <Text>{language.button.submit}</Text>
+        </Pressable> */}
       <LanguageToggle onValueChange={toggleSwitch} isEnabled={isEnabled}/>
     </View>
   );
