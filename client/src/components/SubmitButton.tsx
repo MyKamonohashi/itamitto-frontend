b1e6ca0f13e54998cef6349421958b41cfc17250
nikobatzi1990 <nicole.boci@gmail.com>
2024-03-26 23:32:49 +0900

    chore: add props and types

--- client/src/components/SubmitButton.tsx
+++ client/src/components/SubmitButton.tsx
@@ -1,16 +1,17 @@
 import { Pressable, StyleSheet, Text } from "react-native";
 import { GestureResponderEvent } from "react-native";
+import { LanguageType } from "../type/type";
 
 type SubmitButtonProps = {
-  onPress: GestureResponderEvent,
+  language: LanguageType,
+  onPress: (event: GestureResponderEvent) => void
 }
 
-
-export default function SubmitButton() {
+export default function SubmitButton({language, onPress}: SubmitButtonProps) {
   return (
     <>
-      <Pressable style={styles.submit}>
-        <Text>{}</Text>
+      <Pressable style={styles.submit} onPress={onPress}>
+        <Text>{language.button.submit}</Text>
       </Pressable>
     </>
   );
