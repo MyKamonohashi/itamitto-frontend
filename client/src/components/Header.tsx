315ca4d41a04310cbf072d96755868a85785800d
nikobatzi1990 <nicole.boci@gmail.com>
2024-04-04 23:11:29 +0900

    feat: create head component

---
+++ client/src/components/Header.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native';
+import { useState, useEffect } from 'react';
+
+export default function Header() {
+  return (
+    <>
+      <Text></Text>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+
+})
