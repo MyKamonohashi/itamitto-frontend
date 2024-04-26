dbc7cf0b50c5d334f6fbbaab6bcb590a4bc91aed
nikobatzi1990 <nicole.boci@gmail.com>
2024-04-26 23:38:48 +0900

    style: frameNine buttons

--- client/src/components/FrameNine.tsx
+++ client/src/components/FrameNine.tsx
@@ -40,12 +40,14 @@ export default function FrameNine({ route, navigation }: FrameNineProps) {
   return (
     <View style={styles.container}>
       <Header text={language.title.medications} />
-      <Pressable style={styles.button} onPress={handleTakesMedications}>
-        <Text style={styles.buttonText}>{language.button.yes}</Text>
-      </Pressable>
-      <Pressable style={styles.button} onPress={handleNoMedications}>
-        <Text style={styles.buttonText}>{language.button.no}</Text>
-      </Pressable>
+      <View style={styles.buttons}>  
+        <Pressable style={styles.button} onPress={handleTakesMedications}>
+          <Text style={styles.buttonText}>{language.button.yes}</Text>
+        </Pressable>
+        <Pressable style={styles.button} onPress={handleNoMedications}>
+          <Text style={styles.buttonText}>{language.button.no}</Text>
+        </Pressable>
+      </View>
       { takingMedications && 
         <View>
           <View style={styles.inputs}>
@@ -76,15 +78,19 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'space-around'
   },
+  buttons: {
+    flexDirection: 'row',
+    gap: 30,
+  },
   button: {
     borderRadius: 10,
     borderWidth: 1,
     backgroundColor:'#039388',
-    padding: 20,
-    minWidth: 100,
+    padding: 15,
+    minWidth: 150,
   },
   buttonText: {
-    fontSize: 25,
+    fontSize: 28,
     textAlign: 'center',
     color: 'white'
   },
